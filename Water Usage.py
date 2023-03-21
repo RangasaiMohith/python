22df6c14afefa1f3ab84a5610e56dab7d266c16f
RangasaiMohith <127702660+RangasaiMohith@users.noreply.github.com>
2023-03-20 17:44:53 -0700

    Create Water Usage.py

---
+++ Water Usage.py
@@ -0,0 +1,12 @@
+print (f"Make the world a btter place")
+print(f"How much time do you shower (minutes)")
+t =int(input())
+a = (t * 365)
+print ("Total water so far per year (Gallons):")
+print (a)
+print (f"how long do you water your garden  (hours per month)")
+r =int(input())
+print ("Total water so far per year (Gallons)")
+x=r*12
+y=a+x
+print (y)
